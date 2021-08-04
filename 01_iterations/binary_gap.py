aee512e5efeb6236b51b57de7968f69f834139a4
Ale_lata <nazotrol@gmail.com>
2021-08-05 08:53:23 +0900

    se compara los diferentes binay gaps en la cadena binaria se regresa el valor maximo de binary gap

--- 01_iterations/binary_gap.py
+++ 01_iterations/binary_gap.py
@@ -1,18 +1,22 @@
-decimal_number = 41
+decimal_number = 578
 
 def solution(N):
     binary = format(N,'b')
     print(binary)
     binary_gap = 0
+    max_bg = 0
     for bit in binary:
         if bit == "0":
-            print(f'cero')
             binary_gap = binary_gap + 1
         else:
-            print(f'uno')
+            if binary_gap > max_bg:
+                max_bg = binary_gap
+            print(f'binary gap actual {binary_gap}')
             binary_gap = 0
             print(f'reseteando el binary gap')
+    print(f'este es el maximo binary gap {max_bg}')
     print(binary_gap)
+    return max_bg
 
 
 if __name__=='__main__':
